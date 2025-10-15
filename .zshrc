84cc6977792b36b0bbcbb02edf3eba3862c7f860
Julian Schulz <wuschelschulz8@gmail.com>
2025-10-15 15:48:09 +0100

    Initial commit: ML GPU dotfiles setup

---
+++ .zshrc
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# Source aliases
+source ~/dotfiles/aliases.sh
+
+# Setup authentication
+source ~/dotfiles/auth.sh
+
+# Add bin to PATH
+export PATH="$HOME/dotfiles/bin:$PATH"
+
---
+++ aliases.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# Editor
+alias code="open -a Cursor"
+
+# Navigation
+alias ..="cd .."
+alias ...="cd ../.."
+
+# List
+alias ll="ls -lah"
+
+# Git
+alias gs="git status"
+alias ga="git add ."
+alias gc="git commit -m"
+alias gp="git push"
+alias gl="git log --oneline --graph --decorate"
+
